0fb7e43d2b5ed02d1d558ef1fa064de05b4b903d
sly1061101 <sly1061101@gmail.com>
2018-03-23 22:06:21 -0500

    typo

--- illiniland/urls.py
+++ illiniland/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-	path('', include(forum.urls))
+	path('', include(forum.urls)),
     path('admin/', admin.site.urls),
 ]
