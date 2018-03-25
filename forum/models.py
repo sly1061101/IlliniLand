55ced8b38df23e7f7535b09bca86217a668e0a81
sly1061101 <sly1061101@gmail.com>
2018-03-24 22:08:32 -0500

    Modify models

--- forum/models.py
+++ forum/models.py
@@ -6,56 +6,42 @@ class Department(models.Model):
 	name = models.CharField(max_length=100)
 
 class Student(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	department = models.ForeignKey(Department, on_delete=models.CASCADE)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	name = models.CharField(max_length=100)
+	start_date = models.DateField()
+	end_data = models.DateField()
+	bio = models.TextField()
 
 class Course(models.Model):
-	department = models.ForeignKey(Department, on_delete=models.CASCADE)
 	number = models.IntegerField()
 	title = models.CharField(max_length=100)
-	overall_score = models.FloatField()
-	difficulty = models.FloatField()
-	workload = models.FloatField()
-
-class Forum(models.Model):
-	FIN = models.IntegerField(primary_key = True)
-	course = models.OneToOneField(Course, on_delete=models.CASCADE)
+	department = models.ForeignKey(Department, on_delete=models.CASCADE)
+	description = models.TextField()
 
 class Take(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	course = models.ForeignKey(Course, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Comment(models.Model):
-	CID = models.IntegerField(primary_key = True)
-	title = models.CharField(max_length=100)
-	content = models.TextField()
-	overall_score = models.FloatField()
-	difficulty = models.FloatField()
-	workload = models.FloatField()
-	course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
-class Make(models.Model):
-	comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class Professor(models.Model):
-	PIN = models.IntegerField(primary_key = True)
-	name = models.CharField(max_length=100)
-	department = models.ForeignKey(Department, on_delete=models.CASCADE)
-
-class Instruct(models.Model):
-	professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+	content = models.TextField(default="")
+	overall_score = models.FloatField()
+	difficulty_score = models.FloatField()
+	workload_score = models.FloatField()
+	professor_score = models.FloatField()
 	course = models.ForeignKey(Course, on_delete=models.CASCADE)
+	time = models.DateTimeField(auto_now=True)
 
 class Question(models.Model):
-	QIN = models.IntegerField(primary_key = True)
 	title = models.CharField(max_length=100)
 	content = models.TextField()
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+	time = models.DateTimeField(auto_now=True)
 
 class Answer(models.Model):
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	AIN = models.IntegerField()
 	content = models.TextField()
-	user = models.ForeignKey(User, on_delete=models.CASCADE)	
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	time = models.DateTimeField(auto_now=True)
