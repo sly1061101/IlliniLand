ab9b0fe8b75b3a496e44ce9b30cc9539f32aa38e
sly1061101 <sly1061101@gmail.com>
2018-03-24 21:05:36 -0500

    Add a page to import data into database

--- forum/views.py
+++ forum/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, HttpResponseRedirect
+from forum.models import Department, Course
+import json
 
 # Create your views here.
 
@@ -33,3 +35,24 @@ def profile(request):
 
 def edit_profile(request):
 	return render(request, "forum/edit_profile.html")
+
+def import_data(request):
+	if(request.method == 'POST'):
+		if request.POST['command'] == 'department':
+			with open('/Users/liuyi/Desktop/data.json') as json_file:
+				data = json.load(json_file)
+				for subject in data:
+					department = Department(name = subject)
+					department.save()
+		elif request.POST['command'] == 'course':
+			with open('/Users/liuyi/Desktop/data.json') as json_file:
+				data = json.load(json_file)
+				for subject in data:
+					for number in data[subject]['courses']:
+						title = data[subject]['courses'][number]['course_title']
+						department = Department.objects.get(name=subject)
+						course = Course(number=number, title=title, department=department, overall_score=0, difficulty=0, workload=0)
+						course.save()
+		return HttpResponse("success!")
+	else:
+		return render(request, "forum/import_data.html")
