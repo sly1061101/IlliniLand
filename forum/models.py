b570b22294f516b41c7288cf2cc1e3bbf01a47a7
shirleywang5269 <shirleywang5269@gmail.com>
2018-04-21 17:40:59 -0500

    hh

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Department(models.Model):
+	name = models.CharField(max_length=100)
+
+class Student(models.Model):
+	department = models.ForeignKey(Department, on_delete=models.CASCADE)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	name = models.CharField(max_length=100)
+	start_date = models.DateField()
+	end_data = models.DateField()
+	bio = models.TextField()
+
+class Course(models.Model):
+	number = models.IntegerField()
+	title = models.CharField(max_length=100)
+	department = models.ForeignKey(Department, on_delete=models.CASCADE)
+	description = models.TextField()
+	def get_number_comments(self):
+		all_comments = Comment.objects.filter(course_id=self.id)
+		return len(all_comments)
+	
+	def get_avg_scores(self):
+		all_comments = Comment.objects.filter(course_id=self.id)
+		n_comments = len(all_comments)
+		total_overall = 0
+		total_difficulty = 0
+		total_workload = 0
+		total_professor = 0
+		for comment in all_comments:
+			total_overall += comment.overall_score
+			total_difficulty += comment.difficulty_score
+			total_workload += comment.workload_score
+			total_professor += comment.professor_score
+		if n_comments == 0:
+			n_comments = 1
+		return (total_overall/n_comments, total_difficulty/n_comments, total_workload/n_comments, total_professor/n_comments)
+
+
+class Take(models.Model):
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	content = models.TextField(default="")
+	overall_score = models.FloatField()
+	difficulty_score = models.FloatField()
+	workload_score = models.FloatField()
+	professor_score = models.FloatField()
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
+	time = models.DateTimeField(auto_now=True)
+
+class Question(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	time = models.DateTimeField(auto_now=True)
+	def get_number_answers(self):
+		return len(Answer.objects.filter(question__id=self.id))
+
+class Answer(models.Model):
+	content = models.TextField()
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	time = models.DateTimeField(auto_now=True)
--- forum/urls.py
+++ forum/urls.py
@@ -1,17 +1,17 @@
 """illiniland URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+	https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+	1. Add an import:  from my_app import views
+	2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+	1. Add an import:  from other_app.views import Home
+	2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+	1. Import the include() function: from django.urls import include, path
+	2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -20,9 +20,21 @@ from . import views
 urlpatterns = [
 	path('', views.index),
 	path('login/', auth_views.login),
-	path('logout/', auth_views.logout),
+	path('logout/', auth_views.logout, {'next_page': '/'}),
 	path('register/', views.register),
-    path('user/', views.user),
-    path('profile/', views.profile),
-    path('edit_profile/', views.edit_profile),
+	path('course/<int:course_id>/', views.course),
+	path('course/<int:course_id>/add_comment/', views.add_comment),
+	path('course/<int:course_id>/add_question/', views.add_question),
+	path('square/', views.square),
+	path('user/', views.user),
+	path('user/home/',views.home),
+	path('user/profile/', views.profile),
+	path('user/edit_profile/', views.edit_profile),
+	path('user/addCourse/', views.addCourse),
+	path('question/', views.question),
+	path('question/<int:question_id>/', views.question),
+	path('question/<int:question_id>/new_answer/', views.new_answer),
+	path('import_data/', views.import_data),
+	path('initial_demo/', views.initial_demo),
+	path('template_test/', views.template_test),
 ]
