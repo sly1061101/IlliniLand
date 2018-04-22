bf13da87b672c7c1bc29f90a43620fb2326b298d
sly1061101 <sly1061101@gmail.com>
2018-04-22 18:25:29 -0500

    parameter

--- find_related_questions.py
+++ find_related_questions.py
@@ -110,6 +110,10 @@ if __name__ == '__main__':
 	distance = []
 	for i in range(0, len(all_questions_title)):
 		distance.append(levenshtein(s_query_title + " " + s_query_content, all_questions_title[i] + " " + all_questions_content[i]))
+		# print(levenshtein(s_query_title + " " + s_query_content, all_questions_title[i] + " " + all_questions_content[i]))
+		dis_t = levenshtein(s_query_title, all_questions_title[i])
+		dis_c = levenshtein(s_query_content, all_questions_content[i])
+		distance.append(5*dis_t + dis_c)
 
 	for i in range(0, len(distance)):
 		q.put((distance[i], i))
