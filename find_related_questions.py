cc506ab772176bae76de1528a0e26d6b829e0e95
sly1061101 <sly1061101@gmail.com>
2018-04-22 20:32:26 -0500

    related question

--- find_related_questions.py
+++ find_related_questions.py
@@ -43,10 +43,10 @@ if __name__ == '__main__':
 	doc = metapy.index.Document()
 	doc.content(s_query_title)
 	tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)	
-	tok = metapy.analyzers.LengthFilter(tok, min=2, max=30)	
-	tok = metapy.analyzers.LowercaseFilter(tok)	
-	tok = metapy.analyzers.ListFilter(tok, "lemur-stopwords.txt", metapy.analyzers.ListFilter.Type.Reject)	
-	tok = metapy.analyzers.Porter2Filter(tok)	
+	# tok = metapy.analyzers.LengthFilter(tok, min=2, max=30)	
+	# tok = metapy.analyzers.LowercaseFilter(tok)	
+	# tok = metapy.analyzers.ListFilter(tok, "lemur-stopwords.txt", metapy.analyzers.ListFilter.Type.Reject)	
+	# tok = metapy.analyzers.Porter2Filter(tok)	
 	tok.set_content(doc.content())	
 	tokens = [token for token in tok]	
 	s_query_title = ""	
@@ -60,10 +60,10 @@ if __name__ == '__main__':
 	doc = metapy.index.Document()
 	doc.content(s_query_content)
 	tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)	
-	tok = metapy.analyzers.LengthFilter(tok, min=2, max=30)	
-	tok = metapy.analyzers.LowercaseFilter(tok)	
-	tok = metapy.analyzers.ListFilter(tok, "lemur-stopwords.txt", metapy.analyzers.ListFilter.Type.Reject)	
-	tok = metapy.analyzers.Porter2Filter(tok)	
+	# tok = metapy.analyzers.LengthFilter(tok, min=2, max=30)	
+	# tok = metapy.analyzers.LowercaseFilter(tok)	
+	# tok = metapy.analyzers.ListFilter(tok, "lemur-stopwords.txt", metapy.analyzers.ListFilter.Type.Reject)	
+	# tok = metapy.analyzers.Porter2Filter(tok)	
 	tok.set_content(doc.content())	
 	tokens = [token for token in tok]	
 	s_query_content = ""	
