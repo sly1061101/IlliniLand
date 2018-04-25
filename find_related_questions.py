394ce6a587622d5aeec39afaa7306ff40f087e36
sly1061101 <sly1061101@gmail.com>
2018-04-25 01:55:30 -0500

    Avoid showing itself in related question

--- find_related_questions.py
+++ find_related_questions.py
@@ -9,32 +9,6 @@ import numpy as np
 import queue as Q
 from querytexts import Query
 
-def similarity(seq1, seq2):  
-	size_x = len(seq1) + 1
-	size_y = len(seq2) + 1
-	matrix = np.zeros ((size_x, size_y))
-	for x in range(size_x):
-		matrix [x, 0] = x
-	for y in range(size_y):
-		matrix [0, y] = y
-
-	for x in range(1, size_x):
-		for y in range(1, size_y):
-			if seq1[x-1] == seq2[y-1]:
-				matrix [x,y] = min(
-					matrix[x-1, y] + 1,
-					matrix[x-1, y-1],
-					matrix[x, y-1] + 1
-				)
-			else:
-				matrix [x,y] = min(
-					matrix[x-1,y] + 1,
-					matrix[x-1,y-1] + 1,
-					matrix[x,y-1] + 1
-				)
-	return (matrix[size_x - 1, size_y - 1])
-
-
 if __name__ == '__main__':
 	cwd = os.getcwd()
 	file = open("s_query_title.txt", "r")
@@ -79,8 +53,8 @@ if __name__ == '__main__':
 	print(results)
 
 	file = open("result.txt", "w")
-	cnt = 0
-	while cnt < 3 and results[cnt][1] > 0:
+	cnt = 1
+	while cnt < 4 and results[cnt][1] > 0:
 		file.write(str(results[cnt][0]) + "\n")
 		cnt += 1
 	file.close()
