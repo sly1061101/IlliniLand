207fad639b491d9a027403e64d5b80becda36a99
sly1061101 <sly1061101@gmail.com>
2018-04-25 11:37:29 -0500

    Revert "test"
    This reverts commit c24b80b78ea799a3994c7f5a9a2fbc0b2d6a6553.

--- find_related_questions.py
+++ find_related_questions.py
@@ -42,7 +42,6 @@ if __name__ == '__main__':
 	results = sorted(results.items(), key=operator.itemgetter(1))
 	results.reverse()
 
-	print(results)
 	# for i, r in enumerate(results):
 	# 	results[i] = (r[0], r[1] - results[len(results)-1][1])
 
